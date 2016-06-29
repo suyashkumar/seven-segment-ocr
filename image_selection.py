477b947a800432c5e3ee9b30b67c302d70a3f7d3
Suyash Kumar <suyash.kumar@duke.edu>
2016-06-29 12:43:46 -0400

    Minor updates to selection library, added some error catching

--- image_selection.py
+++ image_selection.py
@@ -1,11 +1,25 @@
+"""
+This module gives the user an image to specify selections
+in and returns those selections
+@author: Suyash Kumar <suyashkumar2003@gmail.com>
+"""
 import cv2
+import sys
 
+# Shared variable declarations
 refPts = []
-doneSelecting = False
 image=1
 numSelected=0
+
+"""
+Called every time a click event is fired on the displayed
+image. Is used to record ROI selections from the user, and
+updates the image to place a bounding box highlighting the
+ROIs
+"""
 def click_handler(event, x, y, flags, pram):
-    global refPts, doneSelecting, image, numSelected
+    global refPts, image, numSelected
+
     if(event == cv2.EVENT_LBUTTONDOWN):
         if(len(refPts)==0):
             refPts = [(x,y)]
@@ -13,7 +27,6 @@ def click_handler(event, x, y, flags, pram):
             refPts.append((x,y))
     elif (event == cv2.EVENT_LBUTTONUP):
         refPts.append((x,y))
-        doneSelecting = True
         cv2.rectangle(image, refPts[numSelected*2], refPts[(numSelected*2)+1], (0,255,0), 2, lineType=8)
         cv2.imshow("image",image)
         numSelected = numSelected+1
@@ -22,6 +35,7 @@ def click_handler(event, x, y, flags, pram):
 def getSelectionsFromImage(img):
     global image, refPts, numSelected
     image = img
+    refPts = [] # Reinit refPts
     clone = image.copy()
     cv2.namedWindow("image")
     cv2.setMouseCallback("image", click_handler)
@@ -38,12 +52,12 @@ def getSelectionsFromImage(img):
     if ((len(refPts) % 2) == 0):
         print len(refPts)/2
         print refPts
-        for x in range(0, len(refPts)/2):
-            print x
-            roi = clone[refPts[0+(x*2)][1]:refPts[1+(x*2)][1], refPts[0+(2*x)][0]:refPts[1+(2*x)][0]]
-            cv2.imshow("ROI"+str(x), roi)
+        for selection in range(0, len(refPts)/2):
+            roi = clone[refPts[0+(selection*2)][1]:refPts[1+(selection*2)][1], refPts[0+(2*selection)][0]:refPts[1+(2*selection)][0]]
+            cv2.imshow("ROI"+str(selection), roi)
     else:
-        print "Even number "
+        sys.exit("Selection Capture didn't get an even number of bounding points.")
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return refPts
